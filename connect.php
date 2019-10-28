4a47a57811b146105bbaa16fd3fe71e64ab91ec0
Eric Badger <ens.badger@gmail.com>
2019-10-28 20:31:41 +0000

    new changes

--- connect.php
+++ connect.php
@@ -1,17 +1,17 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
+//$username = "root";
+//$password = "badger761676";
 //$password = "@pdT,2]eKw?{";
-// $username = "mobilead_mobcyb";
-// $password = "Sr4ywDJ";
+ $username = "cybersecsec";
+ $password = "B3nj!CeaGH9N4";
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=website_formncsc", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=cybersec_ncsc_ncsam_db", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 //    $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-//    echo "Connected successfully";
+    echo "Connected successfully";
     }
 catch(PDOException $e)
     {
